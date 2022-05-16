76eb6014b151a2f81ba23060a5bc439b6ef9aac1
Carlos Mario Espinosa Restrepo <carlosespinosa66@outlook.com>
2022-05-16 15:31:43 -0500

    UPDATE

--- client/src/screens/WeAreScreen.js
+++ client/src/screens/WeAreScreen.js
@@ -1,30 +1,30 @@
-// import React from 'react'
+import React from 'react'
 
-// export default function WeAre() {
-//   return (
-//     <div >We Are Juan Pablo Espinosa and Daniel Betancur</div>
-//   )
-// }
-
-
-
-
-import React from 'react';
-// import 'bootstrap/dist/css/bootstrap.css';
-import Container from 'react-bootstrap/Container';
-  
-export default function App() {
+export default function WeAre() {
   return (
-    <div style={{ display: 'block',
-                  width: 700, padding: 30 }}>
-      <h4>React-Bootstrap Container Component</h4>
-      <Container
-        style={{
-          backgroundColor: 'green'
-        }}
-      >
-        <h3>Sample Text, Greetings from GeeksforGeeks</h3>
-      </Container>
-    </div>
-  );
+    <div >We Are Juan Pablo Espinosa and Daniel Betancur</div>
+  )
 }
+
+
+
+
+// import React from 'react';
+// // import 'bootstrap/dist/css/bootstrap.css';
+// import Container from 'react-bootstrap/Container';
+  
+// export default function App() {
+//   return (
+//     <div style={{ display: 'block',
+//                   width: 700, padding: 30 }}>
+//       <h4>React-Bootstrap Container Component</h4>
+//       <Container
+//         style={{
+//           backgroundColor: 'green'
+//         }}
+//       >
+//         <h3>Sample Text, Greetings from GeeksforGeeks</h3>
+//       </Container>
+//     </div>
+//   );
+// }
